17db0f9f6da2c721a29d1b7f31a9d85bf3ce1a93
hvcsc <jrhcmll13@gmail.com>
2025-03-13 00:03:58 +0800

    pseudocode

---
+++ Batch_2/2.10_in_between.py
@@ -0,0 +1,9 @@
+#NAME: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.10 - IN BETWEEN
+#DATE: MARCH 13, 2025
+
+"""Prog10: Create a program that ask user to input 2 numbers.
+Print all the numbers between the two numbers."""
+
+#input numbers
+#conditions and print
