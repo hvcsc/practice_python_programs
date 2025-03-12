a39079980e56b38b07935f8edfb30251d72aaa0d
hvcsc <jrhcmll13@gmail.com>
2025-03-12 16:38:33 +0800

    pseudocode

---
+++ Batch_1/1.9_even_numbers.py
@@ -0,0 +1,9 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 1.9 - EVEN NUMBERS FROM 0 - 100
+#DATE: MARCH 12, 2025
+
+"""Prog09: Create a program that print all the even
+numbers starting from 0 to 100. (Use for loop)"""
+
+#set for loop
+#print output
