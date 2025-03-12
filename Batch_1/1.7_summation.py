39aa2307d1c01e4c0b57c49919127220c6b90873
hvcsc <jrhcmll13@gmail.com>
2025-03-12 16:18:38 +0800

    pseudocode

---
+++ Batch_1/1.7_summation.py
@@ -0,0 +1,11 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 1.7 - SUMMATION
+#DATE: MARCH 12, 2025
+
+
+"""Prog07: Create a program that ask user to input 10 numbers.
+Print the sum of all the numbers."""
+
+#ask user for numbers
+#add all numbers
+#print summation
