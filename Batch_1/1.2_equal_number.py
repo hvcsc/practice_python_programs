afdb1934c5dd766cf0d39ae5293098ba6912c373
hvcsc <jrhcmll13@gmail.com>
2025-03-12 15:46:59 +0800

    pseudocode

---
+++ Batch_1/1.2_equal_number.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 1.1 - PRINT BIGGER NUMBER
+#DATE: MARCH 12, 2025
+
+"""Prog02: Create a program that ask user to input 2 numbers.
+Print "Equal" when the numbers are the same."""
+
+#input numbers
+#check if equal
+#print output
