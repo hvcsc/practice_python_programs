418b2a01b61273831158d26fddcafa0f0c3afc7a
hvcsc <jrhcmll13@gmail.com>
2025-03-12 15:56:38 +0800

    pseudocode

---
+++ Batch_1/1.4_number_product.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 1.4 - PRODUCT
+#DATE: MARCH 12, 2025
+
+"""Prog04: Create a program that ask user to input 2 numbers.
+Print the product of the two numbers."""
+
+#ask user for numbers
+#product equation
+#print product
