c40a980cc2906374bb168205911ad648c64bac16
hvcsc <jrhcmll13@gmail.com>
2025-03-12 21:46:34 +0800

    pseudocode

---
+++ Batch_2/2.5_remainder.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.5 - REMAINDER
+#DATE: MARCH 12, 2025
+
+"""Prog05: Create a program that ask user to input 2 numbers.
+Print the remainder when the first number is divided by the second number."""
+
+#ask user for numbers
+#remainder equation
+#print remainder
