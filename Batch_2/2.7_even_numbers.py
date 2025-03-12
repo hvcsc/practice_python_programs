889620f0c6d7f86ed5db6aa223e84d5580817121
hvcsc <jrhcmll13@gmail.com>
2025-03-12 23:40:41 +0800

    pseudocode

---
+++ Batch_2/2.7_even_numbers.py
@@ -0,0 +1,11 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.7 - EVEN NUMBERS COUNT
+#DATE: MARCH 12, 2025
+
+"""Prog07: Create a program that ask user to input 10 numbers.
+Print how many are even numbers."""
+
+#ask user for numbers
+#check for even numbers
+#initialize and count how many even numbers
+#print output
