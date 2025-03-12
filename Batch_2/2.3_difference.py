eedc388931309b3f74794a17e961e494b5f8bde9
hvcsc <jrhcmll13@gmail.com>
2025-03-12 20:36:36 +0800

    pseudocode

---
+++ Batch_2/2.3_difference.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.3 - DIFFERENCE
+#DATE: MARCH 12, 2025
+
+"""Prog03: Create a program that ask user to input 2 numbers.
+Print the difference of the two numbers."""
+
+#ask users for numbers
+#equation of difference
+#print difference
