6870ad4d92d4c820cfa6f2ef2d59e8faf8a60165
hvcsc <jrhcmll13@gmail.com>
2025-03-12 19:30:49 +0800

    pseudocode

---
+++ Batch_2/2.2_not_equal_numbers.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.2 - PRINT NOT EQUAL
+#DATE: MARCH 12, 2025
+
+"""Prog02: Create a program that ask user to input 2 numbers.
+Print "Not Equal" when the numbers are not the same."""
+
+#ask user for numbers
+#check if not equal
+#print output
