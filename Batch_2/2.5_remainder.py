28ad24eddea598af7632f2acdf022de837df3d70
hvcsc <jrhcmll13@gmail.com>
2025-03-12 21:55:01 +0800

    restrict zero and print output

--- Batch_2/2.5_remainder.py
+++ Batch_2/2.5_remainder.py
@@ -5,11 +5,21 @@
 """Prog05: Create a program that ask user to input 2 numbers.
 Print the remainder when the first number is divided by the second number."""
 
+#restrict zero denominator
+def zero(prompt):
+    while True:
+        num = float(input(prompt))
+        if num == 0:
+            print("Denominator cannot be zero.")
+        else:
+            return num
+
 #ask user for numbers
 num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num2 = zero("Enter the second number: ")
 
 #remainder equation
 remainder = num1 % num2
 
 #print remainder
+print(f"\nThe remainder when {num1} is divided by {num2} is {remainder}")
