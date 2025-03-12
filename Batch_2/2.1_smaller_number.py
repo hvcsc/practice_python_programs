53c1ba3269c352008c90d264cd301de93e980f31
hvcsc <jrhcmll13@gmail.com>
2025-03-12 19:26:41 +0800

    ask user for numbers

--- Batch_2/2.1_smaller_number.py
+++ Batch_2/2.1_smaller_number.py
@@ -5,5 +5,8 @@
 """Prog01: Create a program that ask user to input 2 numbers. Print the smaller number."""
 
 #ask user for numbers
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
 #check which is smaller
 #print smaller number
