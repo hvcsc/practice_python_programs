04ac87f736d41d7cac4aa4398fd7b7835c96de96
hvcsc <jrhcmll13@gmail.com>
2025-03-12 23:43:21 +0800

    check for even numbers then count

--- Batch_2/2.7_even_numbers.py
+++ Batch_2/2.7_even_numbers.py
@@ -5,10 +5,13 @@
 """Prog07: Create a program that ask user to input 10 numbers.
 Print how many are even numbers."""
 
+count = 0
 #ask user for numbers
 for i in range(10):
     num = float(input(f"Enter number {i + 1}: "))
+    # check for even numbers
+    # initialize and count how many even numbers
+    if num % 2 == 0:
+        count += 1
 
-#check for even numbers
-#initialize and count how many even numbers
 #print output
