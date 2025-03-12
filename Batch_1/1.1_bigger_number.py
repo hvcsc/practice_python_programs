1577d369214b37ded030693df5979c5e71152a7f
hvcsc <jrhcmll13@gmail.com>
2025-03-12 15:35:03 +0800

    pseudocode

---
+++ Batch_1/1.1_bigger_number.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 1.1 - PRINT BIGGER NUMBER
+#DATE: MARCH 12, 2025
+
+"""Prog01: Create a program that ask user to input 2 numbers.
+Print the bigger number."""
+
+#ask user for numbers
+#check which is bigger
+#print output
