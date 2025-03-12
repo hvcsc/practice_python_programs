f86efd43a03800e77e95abc5de57807cd1385618
hvcsc <jrhcmll13@gmail.com>
2025-03-12 16:31:57 +0800

    pseudocode

---
+++ Batch_1/1.8_odd_numbers.py
@@ -0,0 +1,11 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 1.8 - ODD NUMBERS COUNT
+#DATE: MARCH 12, 2025
+
+"""Prog08: Create a program that ask user to input 10 numbers.
+Print how many are odd numbers."""
+
+#ask user for numbers
+#check for odd numbers
+#initialize and count how many odd numbers
+#print output
