ffffbc87f1d986740d39980b6ea952ba89ffe9b3
hvcsc <jrhcmll13@gmail.com>
2025-03-12 16:33:16 +0800

    ask user for numbers

--- Batch_1/1.8_odd_numbers.py
+++ Batch_1/1.8_odd_numbers.py
@@ -6,6 +6,9 @@
 Print how many are odd numbers."""
 
 #ask user for numbers
+for i in range(10):
+    num = float(input(f"Enter number {i + 1}: "))
+
 #check for odd numbers
 #initialize and count how many odd numbers
 #print output
