8ea8f62e29b768f876d769b6934a462a427df8a1
hvcsc <jrhcmll13@gmail.com>
2025-03-13 00:07:50 +0800

    conditions and output

--- Batch_2/2.10_in_between.py
+++ Batch_2/2.10_in_between.py
@@ -6,7 +6,16 @@
 Print all the numbers between the two numbers."""
 
 #input numbers
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 #conditions and print
+print(f"\nNumbers in between of {num1} and {num2}: ")
+if num1 < num2:
+    for num in range(num1 + 1, num2):
+        print(num, end=" ")
+elif num1 > num2:
+    for num in range (num1 - 1, num2, -1):
+        print(num,end=" ")
+else:
+    print("No numbers in between.")
