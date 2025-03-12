2f3ddbdab19168ceeb9ac8ce0cad0a2c97ee8f62
hvcsc <jrhcmll13@gmail.com>
2025-03-12 21:10:00 +0800

    pseudocode

---
+++ Batch_2/2.4_quotient_without_decimal.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.4 - QUOTIENT WITHOUT DECIMALS
+#DATE: MARCH 12, 2025
+
+"""Prog04: Create a program that ask user to input 2 numbers.
+Print the quotient of the two numbers without the decimal point"""
+
+#ask user for numbers
+#division equation
+#print quotient
