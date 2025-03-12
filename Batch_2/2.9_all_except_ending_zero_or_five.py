c5927f8dc4ac1f4cedf70286739d87992e5cb69f
hvcsc <jrhcmll13@gmail.com>
2025-03-12 23:59:15 +0800

    pseudocode

---
+++ Batch_2/2.9_all_except_ending_zero_or_five.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.9 - ALL EXCEPT ENDING IN ZERO OR FIVE
+#DATE: MARCH 12, 2025
+
+
+"""Prog09: Create a program that print all the numbers
+starting from 0 to 100 except numbers ending in zero or ending five."""
+
+#set for loop and condition
+#print output
