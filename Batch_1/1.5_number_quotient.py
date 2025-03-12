1b36de94f975fc52c4f5853da55217a1fe95774b
hvcsc <jrhcmll13@gmail.com>
2025-03-12 16:09:24 +0800

    dividion equation - restrict zero denominator

--- Batch_1/1.5_number_quotient.py
+++ Batch_1/1.5_number_quotient.py
@@ -5,9 +5,20 @@
 """Prog05: Create a program that ask user to input 2 numbers.
 Print the quotient of the two numbers with the decimal point"""
 
+#restrict zero denominator
+def zero(prompt):
+    while True:
+        num = float(input(prompt))
+        if num == 0:
+            print("Denominator cannot be zero.")
+        else:
+            return num
+
 #ask user for numbers
 num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num2 = zero("Enter the second number: ")
 
 #quotient equation
+quotient = num1 / num2
+
 #print quotient
