85e2352745b234f2030bffdf85fe8cc96142541c
hvcsc <jrhcmll13@gmail.com>
2025-03-12 16:35:11 +0800

    check and count odd numbers

--- Batch_1/1.8_odd_numbers.py
+++ Batch_1/1.8_odd_numbers.py
@@ -5,10 +5,13 @@
 """Prog08: Create a program that ask user to input 10 numbers.
 Print how many are odd numbers."""
 
+count = 0
 #ask user for numbers
 for i in range(10):
     num = float(input(f"Enter number {i + 1}: "))
+    #check for odd numbers
+    #initialize and count how many odd numbers
+    if num % 2 != 0:
+        count += 1
 
-#check for odd numbers
-#initialize and count how many odd numbers
 #print output
