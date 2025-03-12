28502ed7e7eb2fb665a9532c3334ff4419a079b4
hvcsc <jrhcmll13@gmail.com>
2025-03-12 16:13:47 +0800

    pseudocode

---
+++ Batch_1/1.6_exponentiation.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 1.6 - EXPONENT
+#DATE: MARCH 12, 2025
+
+"""Prog06: Create a program that ask user to input 2 numbers.
+Print the result when the first number is raised to the second number."""
+
+#ask user for numbers
+#power equation
+#print output
