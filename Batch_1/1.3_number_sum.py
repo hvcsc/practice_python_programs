4130fb590d9487990bd0738b0b4ff1aa5d68980a
hvcsc <jrhcmll13@gmail.com>
2025-03-12 15:51:59 +0800

    pseudocode

---
+++ Batch_1/1.3_number_sum.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 1.3 - SUMMATION
+#DATE: MARCH 12, 2025
+
+"""Prog03: Create a program that ask user to input 2 numbers.
+Print the sum of the two numbers."""
+
+#ask user for numbers
+#sum equation
+#print sum
