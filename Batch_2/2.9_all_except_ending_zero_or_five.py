b5910a01678a0bf904ff9566b846d343d6d9c060
hvcsc <jrhcmll13@gmail.com>
2025-03-13 00:01:02 +0800

    condition and output

--- Batch_2/2.9_all_except_ending_zero_or_five.py
+++ Batch_2/2.9_all_except_ending_zero_or_five.py
@@ -2,9 +2,12 @@
 #PROGRAM: PROG 2.9 - ALL EXCEPT ENDING IN ZERO OR FIVE
 #DATE: MARCH 12, 2025
 
-
 """Prog09: Create a program that print all the numbers
 starting from 0 to 100 except numbers ending in zero or ending five."""
 
 #set for loop and condition
-#print output
+for num in range(101):
+    if num % 10 != 0:
+        if num % 10 != 5:
+            #print output
+            print(num)
