cdab00e9b49d701028f9192d4532dad040b17136
hvcsc <jrhcmll13@gmail.com>
2025-03-12 23:51:43 +0800

    pseudocode

---
+++ Batch_2/2.8_odd_numbers.py
@@ -0,0 +1,11 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.8 - ODD NUMBERS
+#DATE: MARCH 12, 2025
+
+"""Prog08: Create a program that print all the
+odd numbers starting from 0 to 100. (Use while loop)"""
+
+#initialize
+#set while loop
+#print output
+#increment of 2 (ex. num = 1 + increment of 2 = 3 (odd number))
