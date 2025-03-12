f6bdbd6cb8ae63d3fb2c814fd1d4d97d6d85b819
hvcsc <jrhcmll13@gmail.com>
2025-03-12 23:30:02 +0800

    pseudocode

---
+++ Batch_2/2.6_first_minus_all.py
@@ -0,0 +1,12 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.6 - FIRST MINUS ALL
+#DATE: MARCH 12, 2025
+
+
+"""Prog06: Create a program that ask user to input 10 numbers.
+Print the result of the first number minus all of the remaining numbers."""
+
+#ask user for numbers
+#sum of second to last number
+#equation
+#print output
