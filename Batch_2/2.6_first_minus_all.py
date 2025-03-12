4c8bc09fa0eaf1c9009b2a87b0c2cd0ece26a024
hvcsc <jrhcmll13@gmail.com>
2025-03-12 23:32:34 +0800

    ask for numbers

--- Batch_2/2.6_first_minus_all.py
+++ Batch_2/2.6_first_minus_all.py
@@ -7,6 +7,12 @@
 Print the result of the first number minus all of the remaining numbers."""
 
 #ask user for numbers
+num1 = float(input("Enter the first number: "))
+
 #sum of second to last number
+num2 = 0
+for i in range(9):
+    num2 += float(input(f"Enter number {i + 2}: "))
+
 #equation
 #print output
