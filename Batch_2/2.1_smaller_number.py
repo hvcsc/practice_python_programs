eb6e6604c6732198ddaec5cdf9d43569436b2b8c
hvcsc <jrhcmll13@gmail.com>
2025-03-12 19:18:17 +0800

    pseudocode

---
+++ Batch_2/2.1_smaller_number.py
@@ -0,0 +1,9 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 2.1 - PRINT SMALLER NUMBER
+#DATE: MARCH 12, 2025
+
+"""Prog01: Create a program that ask user to input 2 numbers. Print the smaller number."""
+
+#ask user for numbers
+#check which is smaller
+#print smaller number
