a827ef62af11e2d48980affcf67d8c474e86b9f9
hvcsc <jrhcmll13@gmail.com>
2025-03-12 16:02:16 +0800

    pseudocode

---
+++ Batch_1/1.5_number_quotient.py
@@ -0,0 +1,10 @@
+#AUTHOR: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 1.5 - QUOTIENT
+#DATE: MARCH 12, 2025
+
+"""Prog05: Create a program that ask user to input 2 numbers.
+Print the quotient of the two numbers with the decimal point"""
+
+#ask user for numbers
+#quotient equation
+#print quotient
