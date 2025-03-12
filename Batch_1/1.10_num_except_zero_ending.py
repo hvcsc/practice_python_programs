95f4e9195c01ec1ae9cba31a70c5ac8b25c9789a
hvcsc <jrhcmll13@gmail.com>
2025-03-12 16:42:09 +0800

    pseudocode

---
+++ Batch_1/1.10_num_except_zero_ending.py
@@ -0,0 +1,9 @@
+#NAME: ARAG, JIREH CAMILLE S.
+#PROGRAM: PROG 10 - ALL NUMBERS FROM 0-100 EXCEPT ENDING IN ZERO
+#DATE: MARCH 12, 2025
+
+"""Prog10: Create a program that print all the numbers
+starting from 0 to 100 except numbers ending in zero."""
+
+#set loop and condition
+#print output
