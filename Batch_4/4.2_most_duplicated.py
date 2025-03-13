807cbd43509aa8c276a39246bfdf4fb7a43c0508
hvcsc <jrhcmll13@gmail.com>
2025-03-13 19:07:12 +0800

    define function

--- Batch_4/4.2_most_duplicated.py
+++ Batch_4/4.2_most_duplicated.py
@@ -3,8 +3,32 @@ Display the number with the most number of duplicate."""
 
 #define function
 #create an empty dictionary
-#ask for numbers
-#find the highest occurrence
-#print output
+def most_dups():
+    num_count = {}
+    most_dupli = []
+    max_count = 0
+
+    #ask for numbers
+    while True:
+        try:
+            num =int(input("Enter a number: "))
+            if num in num_count:
+                num_count[num] += 1
+            else:
+                num_count[num] = 1
+        except ValueError:
+            break
+
+    #find the highest occurrence
+    if num_count:
+        max_count = max(num_count.values())
+
+        for num in num_count:
+            if num_count[num] == max_count:
+                most_dupli.append(num)
+
+    #print output
+    print(f"Most duplicated number/s: {most_dupli}")
+
 #introduce program
 #call function
