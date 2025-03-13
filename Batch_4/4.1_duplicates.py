58dea1d847350f22a24ce9eb023779af5d34511e
hvcsc <jrhcmll13@gmail.com>
2025-03-13 18:14:31 +0800

    pseudocode

---
+++ Batch_4/4.1_duplicates.py
@@ -0,0 +1,10 @@
+"""Prog01: Create a program that ask user to input 10 numbers.
+Display all numbers that have duplicate."""
+
+#define function
+#create empty lists
+#ask for numbers then add to list
+#check duplicates and add to lost
+#print output
+#introduce the program
+#call function
