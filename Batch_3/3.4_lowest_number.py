2caaae39d18e61c6d406826590929b25b449adfb
hvcsc <jrhcmll13@gmail.com>
2025-03-13 17:34:07 +0800

    pseudocode

---
+++ Batch_3/3.4_lowest_number.py
@@ -0,0 +1,10 @@
+"""Prog04: Create a program that ask user to input a number, continue asking
+until the user input is invalid. Display the lowest number"""
+
+#define function
+#create an empty list to store inputs
+#ask for numbers
+#store input to lost
+#check if input is lower than the number in the list
+#print output
+#call function
