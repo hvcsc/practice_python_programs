cc5de063f4e45ceda86de349c4f4688cca115351
hvcsc <jrhcmll13@gmail.com>
2025-03-13 19:24:16 +0800

    define function

--- Batch_4/4.4_descending_num.py
+++ Batch_4/4.4_descending_num.py
@@ -3,7 +3,23 @@ Display the number from highest to lowest. Clue: sort() function"""
 
 #define function
 #create an empty list
-#ask for numbers then add to list
-#sort and print numbers
+def descending():
+    numbers = []
+
+    #ask for numbers then add to list
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            numbers.append(num)
+        except ValueError:
+            break
+
+    #sort and print numbers
+    if numbers:
+        numbers.sort(reverse=True)
+        print(f"\nNumbers in descending order: {numbers}")
+    else:
+        print("No numerical value entered.")
+        
 #introduce program
 #call function
