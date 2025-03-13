63db0a29d9c1caa73ed8226bca68225f07376249
hvcsc <jrhcmll13@gmail.com>
2025-03-13 17:37:17 +0800

    define function

--- Batch_3/3.4_lowest_number.py
+++ Batch_3/3.4_lowest_number.py
@@ -3,8 +3,21 @@ until the user input is invalid. Display the lowest number"""
 
 #define function
 #create an empty list to store inputs
-#ask for numbers
-#store input to lost
-#check if input is lower than the number in the list
+def lowest_num():
+    lowest = None
+
+    #ask for numbers
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            #store input to lost
+            if lowest is None:
+                lowest = num
+            #check if input is lower than the number in the list
+            elif num < lowest:
+                lowest = num
+        except ValueError:
+            break
+
 #print output
 #call function
