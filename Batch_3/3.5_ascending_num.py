91e63bde2c4ec5698cd6e22ed1e5743db0caf257
hvcsc <jrhcmll13@gmail.com>
2025-03-13 17:54:43 +0800

    pseudocode

---
+++ Batch_3/3.5_ascending_num.py
@@ -0,0 +1,9 @@
+"""Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number
+from lowest to highest. Clue: sort() function"""
+
+#define function
+#create an empty list
+#ask for numbers then add to list
+#sort and print numbers
+#introduce program
+#call function
