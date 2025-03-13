a730ab1bb6f9e419d97c086db91d671c210e6ab2
hvcsc <jrhcmll13@gmail.com>
2025-03-13 09:38:58 +0800

    define and ask user for numbers

--- Batch_3/3.1_no_duplicate.py
+++ Batch_3/3.1_no_duplicate.py
@@ -3,7 +3,16 @@ Display all numbers that don't have duplicate."""
 
 #define unique numbers
 #create an empty list
+def unique():
+    user_input = []
+
 #ask for numbers and add to the list
+    print("Enter 10 numbers:")
+    for i in range(10):
+        num = int(input(f"Enter number {i + 1}: "))
+        #add to the list
+        user_input.append(num)
+
 #create an empty list for unique numbers
 #count number occurrences then add to the unique list
 #print unique numbers
