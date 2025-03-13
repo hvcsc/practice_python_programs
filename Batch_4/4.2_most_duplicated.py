f4fb33047e727a3fce1667770c6b74a04d819e31
hvcsc <jrhcmll13@gmail.com>
2025-03-13 18:55:19 +0800

    pseudocode

---
+++ Batch_4/4.2_most_duplicated.py
@@ -0,0 +1,10 @@
+"""Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid.
+Display the number with the most number of duplicate."""
+
+#define function
+#create an empty dictionary
+#ask for numbers
+#find the highest occurrence
+#print output
+#introduce program
+#call function
