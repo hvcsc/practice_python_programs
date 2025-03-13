c4543119fbb5dc2f37124cc73c21ad15844b3f8b
hvcsc <jrhcmll13@gmail.com>
2025-03-13 17:12:34 +0800

    define function

--- Batch_3/3.3_unique_and_duplicate.py
+++ Batch_3/3.3_unique_and_duplicate.py
@@ -6,7 +6,22 @@ Display "Duplicate" after input when the inputted number have duplicate."""
 #define function
 #restrict non-numerical input
 #create an empty list
-#ask for numbers
-#check if unique or duplicate then print
+def digit():
+    number = []
+    #ask for numbers
+    while True:
+        try:
+            num = int(input("Enter a number: "))
+            #check if the input is in the list
+            if num in digit:
+                print("Duplicate.")
+            else:
+                print("Unique.")
+                number.append(num)
+        except ValueError:
+            print("Invalid input.")
+            break
+            
+#introduce the program
 #call function
 
