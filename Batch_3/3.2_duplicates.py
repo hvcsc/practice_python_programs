d79bf26d198742d9b79eb615e0aa6ed5bc9779d7
hvcsc <jrhcmll13@gmail.com>
2025-03-13 11:43:42 +0800

    ask for numbers

--- Batch_3/3.2_duplicates.py
+++ Batch_3/3.2_duplicates.py
@@ -3,7 +3,15 @@ For numbers with duplicate, display only the first entry."""
 
 #define function
 #create empty lists
+def first_unique():
+    user_input = []
+    display_num = []
+
 #ask for numbers then add to list
+    for i in range(10):
+        num = int(input(f"Enter number {i + 10}: "))
+        user_input.append(num)
+
 #add occurrence to list
 #print output
 #call function
