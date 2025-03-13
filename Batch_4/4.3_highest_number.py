a2a0990a04997151c31af3dc98a7d7949a0e7a37
hvcsc <jrhcmll13@gmail.com>
2025-03-13 19:15:40 +0800

    pseudocode

---
+++ Batch_4/4.3_highest_number.py
@@ -0,0 +1,11 @@
+"""Prog03: Create a program that ask user to input a number,
+continue asking until the user input is invalid. Display the highest number"""
+
+#define function
+#create an empty list to store inputs
+#ask for numbers
+#store input to list
+#check if input is higher than the number in the list
+#print output
+#introduce the program
+#call function
