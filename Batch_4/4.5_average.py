1bde89c6580f337d8931fd24581a8ee3d2b5ab0b
hvcsc <jrhcmll13@gmail.com>
2025-03-13 19:28:02 +0800

    pseudocode

---
+++ Batch_4/4.5_average.py
@@ -0,0 +1,10 @@
+"""Prog05: Create a program that ask user to input a number, continue asking
+until the user input is invalid. Display the average."""
+
+#define function
+#create and empty list
+#initialize
+#ask for numbers
+#calculate average and print
+#introduce program
+#call function
