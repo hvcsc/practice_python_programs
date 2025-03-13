b54772b027848048df2b881f1cbea68b4999d82b
hvcsc <jrhcmll13@gmail.com>
2025-03-13 17:03:02 +0800

    pseudocode

---
+++ Batch_3/3.3_unique_and_duplicate.py
@@ -0,0 +1,12 @@
+"""Prog03: Create a program that ask user to input a number, continue asking
+until the user input is invalid. Display "Unique" after input
+when the inputted number don't have duplicate.
+Display "Duplicate" after input when the inputted number have duplicate."""
+
+#define function
+#restrict non-numerical input
+#create an empty list
+#ask for numbers
+#check if unique or duplicate then print
+#call function
+
