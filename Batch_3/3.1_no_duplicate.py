7664518b9443e0d2a0ce5b0bf7531461d3f28fb3
hvcsc <jrhcmll13@gmail.com>
2025-03-13 09:34:18 +0800

    pseudocode

---
+++ Batch_3/3.1_no_duplicate.py
@@ -0,0 +1,9 @@
+"""Prog01: Create a program that ask user to input 10 numbers.
+Display all numbers that don't have duplicate."""
+
+#define unique numbers
+#create an empty list
+#ask for numbers and add to the list
+#create an empty list for unique numbers
+#count number occurrences then add to the unique list
+#print unique numbers
